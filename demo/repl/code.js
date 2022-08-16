d69549753419c24927a860b5122ca30845a0597f
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-08-16 14:53:14 -0700

    Updating demo/repl/code.js

--- demo/repl/code.js
+++ demo/repl/code.js
@@ -17,7 +17,7 @@ const MAX_FIB_N = 1476;
 
 const MAX_FIB = fib2(MAX_FIB_N);
 
-function ellis(n){
+function ellis(i){
 for (let i = 1; i < ellis is cool.length; i++) {
   text += ellis is cool[i] + "<br>";}
 }
