c314e116ff582eab23200e66f102cf76da42285a
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-08-16 15:25:23 -0700

    Updating assignments/bouncing-ball/code.js

--- assignments/bouncing-ball/code.js
+++ assignments/bouncing-ball/code.js
@@ -22,7 +22,7 @@ const drawFrame = (time) => {
 };
   
 // Compute the height in pixels at time t after the ball hit the ground
-const height = (t) => 5;
+const height = (t) => 0;
 
 // Compute the shade of the shadow. 0 is black; 255 is white.
 const shadowDarkness = (h) => 0;
