f2310816333ba0295afee6be70f192d374b8ced2
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-08-16 20:10:31 -0700

    Updating assignments/bouncing-ball/code.js

--- assignments/bouncing-ball/code.js
+++ assignments/bouncing-ball/code.js
@@ -18,7 +18,7 @@ let ballSize = 15;
 // as the time value that is passed in is just the current time, i.e. the same
 // thing we would get from calling now().
 const drawFrame = (time) => {
-    drawBall(Math.sin(time)*100, 30)
+    drawBall(Math.sin(time)*100+100, 30)
 };
   
 // Compute the height in pixels at time t after the ball hit the ground
