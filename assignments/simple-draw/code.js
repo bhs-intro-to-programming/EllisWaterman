2a32994b647e0f602266933327da2c7c502083ca
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-08-18 15:01:56 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -41,5 +41,5 @@
 drawFilledCircle(width - 400, height - 500, 50, 'green');
 drawFilledCircle(width - 100, height - 500, 50, 'green');
 
-drawFilledRect(400, height - 20, 45, 15, 'purple');
+drawFilledRect(300, height - 20, 45, 15, 'purple');
 
