b11a47d07b899da938dbcaaf5d19444d48b848b8
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-08-26 15:13:02 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -6,12 +6,12 @@
 // number of milliseconds (one millionth of a second) since the program started.
 const drawFrame = (time) => {
   clear();
-  drawFilledCircle((time / 1) % width, height / 2, 50, 'grey');
-  drawFilledCircle((time / 1330) % width, height / 6, 150, 'yellow');
-  drawFilledCircle((time / 4) % width, height / 1, 15, 'blue');
-  drawFilledCircle((time / 5) % width, height / 1, 59, 'green');
-  drawFilledCircle((time / 3) % width, height / 3, 56, 'white');
-  drawFallingTriangle(width / 2, time)
+    drawFilledCircle((time / 1330) % width, height / 6, 150, 'yellow');
+    drawFilledCircle((time / 1) % width, height / 2, 50, 'grey');
+    drawFilledCircle((time / 4) % width, height / 1, 15, 'blue');
+    drawFilledCircle((time / 5) % width, height / 1, 59, 'green');
+    drawFilledCircle((time / 3) % width, height / 3, 56, 'white');
+    drawFallingTriangle(width / 2, time)
 };
 
 // This is a function that we define to make it easier to draw a triangle. You
