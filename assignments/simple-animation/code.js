743c2d0c3a50e7b97a4ae3c479db60e0c27bd9f5
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-08-26 15:11:43 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -37,7 +37,7 @@ const drawFallingTriangle = (x, time) => {
   let y3 = y1 - 90;
 
   // Actually draw the triangle.
-  drawTriangle(x1, y1, x2, y2, x3, y3, 'red', 8);
+  drawTriangle(x1, y1, x2, y2, x3, y3, 'red', 0);
 };
 
 // Leave this code here or the animation won't run. Also don't change the name
