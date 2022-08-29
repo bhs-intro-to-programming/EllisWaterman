61df528e477bc92687f51d74d6c8d81748c5e1e1
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-08-29 14:52:04 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -6,7 +6,7 @@
 // number of milliseconds (one millionth of a second) since the program started.
 const drawFrame = (time) => {
   clear();
-    drawFilledCircle((sin / 1330) % width, height / 6, 150, 'yellow');
+    drawFilledCircle((time / 1330) % width, height / 6, 150, 'yellow');
     drawFilledCircle((time / 1) % width, height / 2, 50, 'grey');
     drawFilledCircle((time / 5) % width, height / 1.2, 59, 'green');
     drawFilledCircle((time / 5) % width, height / 1.3, 20, 'blue'); 
