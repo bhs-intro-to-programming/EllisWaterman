910f855e998738cf96c43c6ed824d383cbe914f5
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-08-29 15:08:38 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -12,7 +12,7 @@ const drawFrame = (time) => {
     drawFilledCircle((time / 5) % width, height / 0, 20, 'blue'); 
     drawFilledCircle((time / 3) % width, height / 0, 56, 'brown');
     drawFallingTriangle(width / 2, time)
-drawLine( 100, 250, 500, 200, 'red');
+drawLine( 100, 250, 5000, 200, 'red');
 
 };
 
