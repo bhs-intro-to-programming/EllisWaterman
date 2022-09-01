7cc44dad8dc69c3486995507c7ec4c7c233beb33
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-01 15:12:20 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -40,7 +40,7 @@ const drawFallingTriangle = (x, time) => {
   let y3 = y1 - 90;
 
   // Actually draw the triangle.
-  drawTriangle(x1, y1, x2, y2, x3, y3, 'white', 3);
+  drawTriangle(x1, y1, x2, y2, x3, y3, 'black', 3);
 };
 // Leave this code here or the animation won't run. Also don't change the name
 // of drawFrame either here or where it is defined. (Or, if you must, change it
