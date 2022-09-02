1cdc05392b4f10ffe77252043547079cd9ec5eb2
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-02 14:59:40 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -20,7 +20,6 @@ drawLine( 0, 134, 4000, 134, 'black');
 drawLine( 0, 120, 4000, 120, 'black');
 drawLine( 0, 148, 4000, 148, 'black');
 drawLine( 0, 160, 4000, 160, 'black');
-    drawFallingCircle(width / 2, time)
 
 };
 
@@ -28,7 +27,7 @@ drawLine( 0, 160, 4000, 160, 'black');
 // This is a function that we define to make it easier to draw a triangle. You
 // may want to experiment with writing your own functions using this one as a
 // model to draw other shapes.
-const drawCircle = (x1, y1, x2, y2, x3, y3, color, width = 0) => {
+const drawTriangle = (x1, y1, x2, y2, x3, y3, color, width = 0) => {
   drawLine(x1, y1, x2, y2, color, width);
   drawLine(x2, y2, x3, y3, color, width);
   drawLine(x3, y3, x1, y1, color, width);
@@ -36,7 +35,7 @@ const drawCircle = (x1, y1, x2, y2, x3, y3, color, width = 0) => {
 
 // This draws a falling trangle of a particular shape whose bottom point is
 // positioned at x and whose y is a function of time.
-const drawFallingCircle = (y, time) => {
+const drawFallingTriangle = (y, time) => {
   // Figure out the x values relative to the passed in x
   let x1 = y - 50;
   let x2 = y;
@@ -48,7 +47,7 @@ const drawFallingCircle = (y, time) => {
   let y3 = y1 - 90;
 
   // Actually draw the triangle.
-  drawTriangle(x1, y1, x2, y2, x3, y3, 'black', 0);
+  drawTriangle(x1, y1, x2, y2, x3, y3, 'black', 3);
 };
 // Leave this code here or the animation won't run. Also don't change the name
 // of drawFrame either here or where it is defined. (Or, if you must, change it
