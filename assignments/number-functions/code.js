f84d5d16251ff826fa46b4aa1e19681070038ba0
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-06 15:20:09 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -2,7 +2,7 @@ const add=(a,b) => a+b
 const subtract=(a,b) => a-b
 const multiply=(a,b) => a*b
 const divide=(a,b) => a/b
-const mod=(a,b,c) => c(a-b)
+const mod=(a,b) => c(a-b)
 const averageOfTwo=(a,b) => (a+b)/2
 const averageOfThree=(a,b,c) => (a+b+c)/3
 const distance=(a,b) => Math.abs(a-b)
