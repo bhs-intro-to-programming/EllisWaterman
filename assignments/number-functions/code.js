ef50cf36fb28b3254e9781cf01b5ba36a933b817
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-06 15:05:13 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -5,4 +5,4 @@ const divide=(a,b) => a/b
 const averageOfTwo=(a,b) => (a+b)/2
 const averageOfThree=(a,b,c) => (a+b+c)/3
 const distance=(a,b) => Math.abs(a-b)
-const manhattanDistance=(x1,x2,y1,y2) => Math.abs(x1-x2)+Math.abs(y1-y2)
+const manhattanDistance=(x1,x2,y1,y2) => Math.abs(x1-y1)+Math.abs(y2-x2)
