07c86483f43c38e8a3f1bdc1e7230742557c9655
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-07 15:25:28 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -30,3 +30,6 @@ const eatsMice=(red, spotted) => !red || !spotted
 const isRed=(s1) => {
 return s1 === 'Flobby' || s1 === 'Bloggy'
 }
+const isSpotted=(s1) => {
+return s1 ==='Flobby' || s1 === 'Flibble'
+}
