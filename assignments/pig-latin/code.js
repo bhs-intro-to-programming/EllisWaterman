e635438ecb54a3a90352306f3757080776b68294
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-12 19:28:33 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -34,7 +34,7 @@
 
 const pigLatin = (s) => s.substring(s.search(/[aeiou]/)) + s.substring(0, s.search((/[aeiou]/))) + 'ay';
 
-const advancedPigLatin = (s) => s === 0 ? s+"way" : pigLatin;
+//const advancedPigLatin = (s) => s === 0 ? s+"way" : pigLatin;
    
 
 
