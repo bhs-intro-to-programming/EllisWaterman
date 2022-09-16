bdbcf491b625cb2ea16837db3df36f34979e3465
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-16 15:29:36 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -54,7 +54,7 @@ const lineOfCircles = () => {
 const lineOfCirclesAlternatingColors = (x) => {
 
   for (let x = 0; x < 49.5; x++)
-    if (x += 2) {
+    if (x += 5) {
       if (x > length)
       drawFilledCircle(x * 10, height / 2, 5, 'blue')
     }
