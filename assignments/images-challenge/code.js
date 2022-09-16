c7f800857742b1093260077e21641e7d57c9e99c
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-16 14:36:45 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -42,7 +42,7 @@ drawFilledCircle(250,300, 30, 'blue')
 const concentricCircle = () => {
   for (let x = 10; x < 45; x++)
     if (width > x * 10) {
-      drawCircle(width/2, height/2, x * 4, 'red', 1)
+      drawCircle(width/2, height/2, x * 2, 'red', 1)
     }
 }
 
