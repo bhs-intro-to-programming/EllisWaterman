66fa56463d353810d67e57202b8e7a2f6e2df52e
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-20 15:00:28 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -66,7 +66,7 @@ const fillWithCircles = () => {
     for (let p = 10; p < 590; p++) {
       let y = (p % 2 === 1)
       if (x > length)
-        drawCircle( p-100, height - p, 5, 'blue', 1)
+        drawCircle(p*10, height - p, 5, 'blue', 1)
     }
   }
 }
