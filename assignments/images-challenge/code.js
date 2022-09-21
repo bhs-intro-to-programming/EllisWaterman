defb2bc9196b7696ad40432f16228cda5b54751c
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-21 16:48:45 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -70,7 +70,7 @@ const fillWithCircles = () => {
   let spacing = 10;
   for (let rows = 0; rows < xsize; rows++) {
     for (let cols = 1; cols < ysize; cols++) {
-      drawCircle((rows * (radius*2)) + xoffset, cols * spacing, radius, 'blue', 1)
+      drawCircle((rows * (radius*2)) + xoffset, cols * (radius*2), radius, 'blue', 1)
     }
   }
 }
