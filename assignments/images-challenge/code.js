8715d7c01b7581c36c1289db286e6195d83bc482
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-21 16:22:44 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -76,7 +76,7 @@ const fillWithRandomFilledCircles = () => {
 
   for (let x = 0; x < 49.5; x++) {
     for (let p = 1; p < 50; p++) {
-      if (height - p < Math.random()) {
+      if (height  < Math.random()) {
       if (x > length)
         drawFilledCircle(x * 10, height - p * 12, 5, 'blue')
         else 
