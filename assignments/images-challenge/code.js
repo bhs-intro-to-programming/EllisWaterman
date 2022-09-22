def4753e62e0105d48a11769de8321ed0cd6f613
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-22 14:37:29 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -95,7 +95,7 @@ let radius = 25;
   let xsize = width / (radius * 2)
   let ysize = height / (radius * 2)
   for (let rows = 0; rows < xsize; rows++) {
-    for (let cols = 0; cols < ysize; cols) {
+    for (let cols = 0; cols < ysize; cols++) {
       drawCircle(rows * (radius * 2) + radius, cols * radius * 2 + radius, radius, 'blue', 1)
     }
   }
