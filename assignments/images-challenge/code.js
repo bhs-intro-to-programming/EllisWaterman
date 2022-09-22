36d39e0319b24750b1be8f8f8120ba6611be5ad8
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-22 14:42:09 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -91,15 +91,15 @@ const fillWithRandomlyFilledCircles = () => {
 //fillWithRandomlyFilledCircles();
 
 const squareOfCircles = () => {
-let r = 25;
+  let r = 25;
   let xsize = width / (r * 2)
   let ysize = height / (r * 2)
   for (let rows = 0; rows < xsize; rows++) {
-    let (cols = 0) 
+    for (let cols = 0; cols < ysize; cols++) {
       drawCircle(rows * (r * 2) + r, cols * r * 2 + r, r, 'blue', 1)
     }
+  }
 }
-
 squareOfCircles();
 
 
