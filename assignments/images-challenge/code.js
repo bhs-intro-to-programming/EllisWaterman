f2bbad41e16074c5981308b5c8ff1b8c83a88a42
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-23 15:15:56 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -113,9 +113,9 @@ const squareOfCircles = () => {
 const checkerboard = () => {
   let s = 25
   for (let p = 0; p < 300; p++)
-   let color = (p % 2 === 1) ? 'blue' : 'red'
+   let color = (p % 2 === 1) ? 'blue' : 'red'; {
   drawFilledRect(p, p, s, s, color)
 }
-
+}
 
 checkerboard();
