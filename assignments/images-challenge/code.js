7a429f95ffba604dac3b0a42d8d987ab997c256c
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-23 15:30:53 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -115,7 +115,7 @@ const checkerboard = () => {
     for (let rows = 0; rows < s; rows++) {
       for (let cols = 0; cols <  100; cols++) {
         let color = (cols % 2 === 1) ? 'blue' : 'red';
-        drawFilledRect(rows*10, cols*10, s, s, color)
+        drawFilledRect(rows*50, cols*10, s, s, color)
       }
     }
   }
