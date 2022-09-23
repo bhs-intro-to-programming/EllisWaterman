72105c62a81b5823d2e7b909e9f3370fcf29e27f
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-23 14:56:35 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -96,7 +96,7 @@ const squareOfCircles = () => {
   let ysize = height / (r * 2)
   for (let rows = 0; rows < xsize; rows++) {
     for (let cols = 0; cols < ysize; cols++) {
-      if (cols-1 < 0)
+      if (cols-1 = 0)
       if (rows-1 < 0)
         drawCircle(rows * (r * 2) + r, cols * r * 2 + r, r, 'blue', 1)
     }
