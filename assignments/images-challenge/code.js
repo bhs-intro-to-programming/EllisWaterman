d58af8e3810daa6c5c3ec7c59dec10cefe757f83
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-27 15:04:25 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -131,6 +131,6 @@ const checkerboard = () => {
 
 const notReallyCurved = () => {
   let a = 0
-    drawLine(a, length, a, height, 'black', 1)
+    drawLine(a, length, a+20, height, 'black', 1)
 }
  notReallyCurved()
