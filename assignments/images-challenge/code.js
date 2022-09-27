dc494a41474dc7d4d0cf152e5ed1b95b28e2d98c
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-27 14:46:29 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -114,8 +114,8 @@ const squareOfCircles = () => {
 
 const checkerboard = () => {
   let s = 25
-  let colsize = width / (s * 2)
-  let rowsize = length / (s * 2)
+  let colsize = Math.floor(width / (s * 2))
+  let rowsize = Math.floor(length / (s * 2))
     for (let rows = 0; rows < rowsize; rows++) {
       for (let cols = 0; cols < colsize; cols++) {
         let color = (cols % 2 === 0) ? 'blue' : 'red';
