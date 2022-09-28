6fe51047c4a3e014e68b9e88ca4421b2bd2e99c3
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-28 14:37:52 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -29,7 +29,7 @@
 const concentricCircle = () => {
   for (let x = 0; x < width; x++) {
     let colour = (x % 2 === 1 ? 'blue' : 'red')
-    drawCircle(width / 2, height, x, colour, 1)
+    drawCircle(width / 2, height / 2, x*10, colour, 1)
   }
 
 }
