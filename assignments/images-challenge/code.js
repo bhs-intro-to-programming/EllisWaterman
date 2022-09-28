04247b6113857b941ce787216efffa3f1a1a4a6a
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-28 15:23:53 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -48,7 +48,7 @@ const concentricCircle = () => {
 const lineOfCircles = () => {
 
   for (let x = 0; x < width; x++)
-    if (x > length-1)
+    if (x > length-10)
       drawFilledCircle(x*10, height / 2, 5, 'red')
 }
 lineOfCircles();
