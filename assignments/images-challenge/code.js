b05ec56fc1c2bf57b5b147075ee0aad1a355aa5f
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-27 18:36:51 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -27,18 +27,18 @@
 
 
 const concentricCircle = () => {
- for (let x = 0; x < length; x++)
- let colour = (x % 2 === 1 ? 'blue' : 'red')
- drawCircle(width/2, height / 2, x, colour, 1)
-
+  for (let x = 0; x < length; x++) {
+    let colour = (x % 2 === 1 ? 'blue' : 'red')
+    drawCircle(width / 2, height / 2, x, colour, 1)
+  }
 
 }
- // for (let x = 0; x < 45; x++) {
-  //   let colour = (x % 2 === 1) ? 'blue' : 'red';
-  //   drawCircle(width / 2, height / 2, x * 5.5, colour, 1)
-  //   if (x > length)
-  //     drawCircle(width / 2, height / 2, x * 5.5, colour, 6)
-  // }
+// for (let x = 0; x < 45; x++) {
+//   let colour = (x % 2 === 1) ? 'blue' : 'red';
+//   drawCircle(width / 2, height / 2, x * 5.5, colour, 1)
+//   if (x > length)
+//     drawCircle(width / 2, height / 2, x * 5.5, colour, 6)
+// }
 concentricCircle();
 
 const lineOfCircles = () => {
