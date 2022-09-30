7dda59d6d42d834875091c1c3f014bcea93324a2
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-09-30 14:51:36 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -16,5 +16,8 @@ const hypotenuse = (a, b) => {
 // ex. hypotenuse(3,4) will return 5
 
 const maxRadius = (heightOfDrawingArea, width) => {
+  if (width < heightOfDrawingArea)
   return heightOfDrawingArea/2
+  else
+  return width/2
 }
