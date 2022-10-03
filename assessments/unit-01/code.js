be17dc0f88fb153c097c0e5efa46aa69ce7e1802
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-03 15:01:44 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -6,24 +6,24 @@ const emit = (a, b, product) => {
 
 // Write your code here ...
 const avarageWeight = (totalWeight, numberOfItems) => {
-  return totalWeight/numberOfItems
+  return totalWeight / numberOfItems
 }
 // put values in ()
 const hypotenuse = (a, b) => {
-  return Math.sqrt(a**2+b**2)
+  return Math.sqrt(a ** 2 + b ** 2)
 }
 //plug in a and b value in ()
 // ex. hypotenuse(3,4) will return 5
 
 const maxRadius = (heightOfDrawingArea, width) => {
   if (width > heightOfDrawingArea)
-  return heightOfDrawingArea/2
+    return heightOfDrawingArea / 2
   else
-  return width/2
+    return width / 2
 }
 
 const numCircles = (radius, width) => {
-   return width/(radius*2)
+  return width / (radius * 2)
 }
 
 const offset = (widthOfArea, widthOfFigure) => {
@@ -35,41 +35,32 @@ const canSleepIn = (weekday, vacation) => {
 }
 
 const canGoToProm = (isSenior, invitedBySenior, promExclusionList) => {
- return isSenior || invitedBySenior && !promExclusionList
+  return isSenior || invitedBySenior && !promExclusionList
 }
 
 const getsSpeedingTicket = (mph, grouchyCop) => {
-  if (grouchyCop == true)
-    if (mph > 65)
-    return true
-    else
-    return false
-  if (grouchyCop == false)
-    if (mph > 70)
-    return true
-    else
-    return false
+  return mph > 70 || grouchyCop && mph > 65;
 }
 
 const moreThanTwiceAsLong = (x, y) => {
-let a = s.length(x)
-let b = s.length(y)
-if (2*a > b)
-return true
-else
-return false
+  let a = s.length(x)
+  let b = s.length(y)
+  if (2 * a > b)
+    return true
+  else
+    return false
 }
 
 const aFartherThanB = (a, b, c) => {
-//noTime = true
+  //noTime = true
 }
 
 const firstHalf = (s) => {
- return s.substring(0, s.length/2)
+  return s.substring(0, s.length / 2)
 }
 
 const secondHalf = (s) => {
-  return s.substring(s.length/2)
+  return s.substring(s.length / 2)
 }
 
 const upDown = () => {
