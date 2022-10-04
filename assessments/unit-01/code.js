df9ddaf24a5ba968cb2547eeff0263135001ac97
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-04 15:09:47 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -89,7 +89,7 @@ const yesIfEven =(x) => {
 }
 
 const countXs = (s) => {
-  let xstring = s.substring(s.search(/[abcddefghijklmnopqrstuvwyz]/),s.search(/[abcddefghijklmnopqrstuvwyz]/))
+  let xstring = s.substring(s.search(/[abcddefghijklmnopqrstuvwyz]/),s.search(/[abcddefghijklmnopqrstuvwyz]/)+1)
   //s.substring(s.search(/[x]/))+s.substring(
  return xstring
 
