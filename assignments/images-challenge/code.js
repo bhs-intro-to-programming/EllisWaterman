84e3da5ab542d03d27ccd110eea088df29727dc5
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-06 14:51:31 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -127,7 +127,7 @@ const fillWithRandomlyColoredFilledCircles = () => {
     }
   }
 }
- fillWithRandomlyColoredFilledCircles()
+ //fillWithRandomlyColoredFilledCircles()
 
 
   const myDrawCircle = (r, rows, cols) => {
