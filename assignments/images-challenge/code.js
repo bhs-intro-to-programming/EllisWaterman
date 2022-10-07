6df9b0bfb69ab3dad715f86591f59bc78df28c16
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-07 15:31:02 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -196,17 +196,6 @@ const fillWithRandomlyColoredFilledCircles = () => {
 
   }
 
-const notQuiteCircle = () => {
-      let a = 25
-    let s = 15
-    for (let y = 0; y < a; y++) {
-      for (let x = 0; x < a; x++) {
-        drawLine(0, y * s, y * s, height - 1, 'black', 1)
-      }
-    }
-  }
-
- notQuiteCircle();
 // colorfulCurve();
 // concentricCircle();
 // lineOfCirclesAlternatingColors();
