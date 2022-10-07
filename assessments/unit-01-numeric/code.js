9bf002a24d5da8c06753f618f132d2b59f111768
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-07 14:59:04 -0700

    Updating assessments/unit-01-numeric/code.js

--- assessments/unit-01-numeric/code.js
+++ assessments/unit-01-numeric/code.js
@@ -7,3 +7,34 @@ const G = 6.6743e-11;
 // syntactically correct function skeleton, more credit for a skeleton with a
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
+
+const itemsLeftOver = (numberOfPeople, numberOfItems) => {
+  return numberOfItems % numberOfPeople
+}
+
+const areaOfCircle = (radius) => {
+  return Math.PI*(radius**2)
+}
+
+const volumeOfCube = (lengthOfEdge) => {
+  return lengthOfEdge**3
+}
+
+const populationGrowth = (currentSize, growthRate) => {
+  return (growthRate*currentSize)+currentSize
+}
+
+const earnedRunAvarage = (earnedRuns, inningsPitched) => {
+  return (earnedRuns/inningsPitched)*9
+}
+
+const valueOfJewels = (numbDiamonds, numbEmeralds, valueOfDiamondInGold, valueOfEmeraldInGold) => {
+  return (numbDiamonds*valueOfDiamondInGold)+(numbEmeralds*valueOfEmeraldInGold)
+}
+
+const  payWithOvertime = (numberOfHoursWorked, hourlyRate, overtimeRate) => {
+  if (numberOfHoursWorked <= 8)
+  return numberOfHoursWorked*hourlyRate
+  else
+  return numberOfHoursWorked*overtimeRate
+}
