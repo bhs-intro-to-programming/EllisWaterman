46e732f59596bd761abb33c5399504ca4b900264
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-12 15:25:03 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -203,6 +203,7 @@ const drawPlatformerGameRandomGenLevel = () => {
       drawFilledRect(yposition, xposition, blockSize, 'black')
     }
   }
+}.
 drawPlatformerGameRandomGenLevel();
 
 // colorfulCurve();
