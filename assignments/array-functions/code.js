9526f79424b47092ed6da7831bf59eb270a8a48e
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-13 15:23:42 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,9 +1,9 @@
-function countTens (x) {
-      let totalTens = 0
+function countTens(x) {
+  let totalTens = 0
   while (countTens) {
     if (x.pop() === 10)
-    totalTens++
-   
+      totalTens++
+    return totalTens
+,
   }
-  return totalTens.
 }
