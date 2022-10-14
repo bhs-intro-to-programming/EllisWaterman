c3a3784abd025209451b2cc3808eb3b545a5e098
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-14 15:27:05 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -19,7 +19,7 @@ function evens(x) {
   const xs = []
   for (let position = 0; position < x.length; position++) {
     if (x[position] % 2 === 0) {
-    xs.push[position]
+    xs.push[x[position]]
     }
   }
   return xs
