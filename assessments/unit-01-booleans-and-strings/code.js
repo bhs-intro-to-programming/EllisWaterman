5842b375c32396edbdccf3d3317a4bf75f223aa5
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-17 15:29:22 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -61,7 +61,7 @@ const randomCharacter = (s) => {
 }
 
 const randomCharacterUpDown = (s) => {
-  if (rand() == 1)
+  if (rand())
   return s.substring(rand(s.length), rand(s.length+1)).toUpperCase()
   else
   return s.substring(rand(s.length), rand(s.length+1)).toLowerCase()
