fcc7ba075d597a070feb09145a9df30643c219b0
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-17 15:11:40 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -33,10 +33,11 @@ const evenGreaterThanZero = (x) => {
 
 const isLeapYear = (year) => {
   if (year % 4 === 0)
-    if(year % 400 === 0)
     return true
-    else
-  
+  if (year % 400 === 0)
+    return true
+  if (year % 100 === 0)
+    return false
+  else
     return false
 }
-
