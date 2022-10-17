9c037d20ff4b3304738a9dd29e0f9714e2e4f4e9
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-17 15:25:14 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -56,5 +56,5 @@ const simplePigLatin = (s, posOfFirstVowel) => {
 }
 
 const randomCharacter = (s) => {
-  return s.substring(rand(s.length), rand(s.length)+1)
+  return s.substring(rand(s.length), rand(s.length)-1)
 }
