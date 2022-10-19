b46ebbd53b2296e75b632ff418d25721212178cb
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-19 14:54:51 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -30,7 +30,7 @@ function anyOverOneHundred(x) {
   for (let numbOver100 = 0; numbOver100 > 100; numbOver100++) {
     if (x.indexOf(numbOver100) >= 0)
       return true
-  }.
+  }
   return false
 }
 
