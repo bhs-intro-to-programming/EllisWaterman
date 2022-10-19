8218bc191d894804de0262ce5ccc229c31628df1
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-19 14:41:03 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -27,7 +27,7 @@ function evens(x) {
 }
 
 function anyOverOneHundred(x) {
-  for (let numbOver100 = 100; numbOver100 < 10000000; numbOver100++) {
+  for (let numbOver100 = 101; numbOver100 < 10000000; numbOver100++) {
     if (x.indexOf(numbOver100) >= 0)
       return true
     else
