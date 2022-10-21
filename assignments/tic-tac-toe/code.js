78ec82c9737939413d5bcfb8bdfe576d8f67eeda
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-21 15:15:51 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -5,14 +5,17 @@
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
 let everyOtherClick = 0
-for(let everyOtherClick = 0; everyOtherClick < 10;) {
-  registerOnclick(() => {
-    everyOtherClick++;
-  });.
-}
+// for(let everyOtherClick = 0; everyOtherClick < 10;) {
+//   registerOnclick(() => {
+//     everyOtherClick++;
+//   });
+// }
 
 registerOnclick((x, y) => {
   drawText('X', x, y, 'black', Math.min(width, height) * 0.3);
+  if (clickCount < 10)
+  clickCount++.
+  console.log(clickCount)
 });
 
 registerOnclick((x, y) => {
