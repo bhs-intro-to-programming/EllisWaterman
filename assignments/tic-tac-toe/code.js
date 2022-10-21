4e9088769e8b5ce512a4b8446494545d453bb2d9
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-21 15:06:44 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -9,6 +9,6 @@ registerOnclick((x, y) => {
   drawText('X', x, y, 'black', Math.min(width, height) * 0.3);
 });
 
-registerOnclick((x, y) => {
+registerOnRightclick((x, y) => {
   drawText('O', x, y, 'black', Math.min(width, height) * 0.3);
 });
