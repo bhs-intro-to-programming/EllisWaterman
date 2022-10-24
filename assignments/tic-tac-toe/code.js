e1b5d46e6b36417eb3f736534807c0218e891be3
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-24 15:10:45 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -4,6 +4,8 @@
 // of the equals sign in our normal `const foo = ...` function definition. This
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
+//drawLine(x1, y1, x2, y2, color, lineWidth)
+
 drawText('#', width/3, height, 'black', Math.min(width, height) * 1.5)
 let clickCount = -1
 drawLine(100, 100, 100, 100, 'black', 5)
