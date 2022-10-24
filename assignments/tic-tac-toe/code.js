c702c92060b7ec705a7e51b236618454550e1b3a
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-24 14:52:24 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -4,8 +4,8 @@
 // of the equals sign in our normal `const foo = ...` function definition. This
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
- let clickCount = 0
- registerOnclick((clickCount) => {
+let clickCount = 0
+ registerOnclick(() => {
   clickCount++;
   console.log(clickCount)
 });
