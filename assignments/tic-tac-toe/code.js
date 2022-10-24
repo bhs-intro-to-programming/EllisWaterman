5bbea6967f6664058bd5318705c796282e312ced
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-24 15:13:12 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -8,7 +8,7 @@
 
 //drawText('#', width/3, height, 'black', Math.min(width, height) * 1.5)
 let clickCount = -1
-drawLine(width, 0, 0, 0, 'black', 5)
+drawLine(width/2, 0, 0, 0, 'black', 5)
 registerOnclick((x, y) => {
   clickCount++
    if (clickCount % 2 === 0)
