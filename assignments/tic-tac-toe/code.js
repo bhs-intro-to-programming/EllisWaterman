aebf9fd4cadcf737c4113331f5c8ea9f293b7508
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-24 15:28:34 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -16,8 +16,8 @@ drawLine(width, height/3*2, 0, height/3*2, 'black', 10)
 drawFilledRect(0, 0, width/3, width/3/3, 'black')
 }
 drawFrame()
-let quad1
-let quad2
+let quad1 = width/3, width/3/3
+let quad2 = 
 let quad3
 let quad4
 let quad5
