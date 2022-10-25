739186048d9d0ff78eeddf6ad4058c4989b7f4c7
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-25 15:21:44 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -21,6 +21,12 @@ registerOnclick((x, y) => {
   drawText('X', width*1/6-50 , height*3/6+25 , 'red', Math.min(width, height) * 0.3);
   else
   drawText('O', width*1/6-50, height*3/6+25, 'blue', Math.min(width, height) * 0.3);
+   else
+   if (y < height && x < width/3)
+   if (clickCount % 2 === 0)
+  drawText('X', width*1/6-50 , height+25 , 'red', Math.min(width, height) * 0.3);
+  else
+  drawText('O', width*1/6-50, height+25, 'blue', Math.min(width, height) * 0.3);
 });
 const drawFrame = () => {
 drawLine(width/3, height, width/3, 0, 'black', 10)
