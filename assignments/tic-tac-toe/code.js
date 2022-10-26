a161763fd160bd93c1d6e68486a3fc57f92fbbad
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-26 15:00:58 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,3 +1,12 @@
+// This is a bit of a new thing. registerOnclick is a function provided by the
+// framework. But the argument we're passing to it is *another* function. Notice
+// how the argument here looks like what we normally put on the righthand side
+// of the equals sign in our normal `const foo = ...` function definition. This
+// is called an anonymous function. We'll discuss this in more detail in a few
+// weeks but for now you can just adapt this code.
+//drawLine(x1, y1, x2, y2, color, lineWidth)
+ 
+//drawText('#', width/3, height, 'black', Math.min(width, height) * 1.5)
 let clickCount = -1
 let shape = 'O'
 let colour = 'red'
@@ -18,19 +27,19 @@ registerOnclick((x, y) => {
  
   else
   if (y < height/3*2 && x < width/3)
- drawText(shape, width*1/6-50 , height*3/6+25 , 'red', Math.min(width, height) * 0.3);
+ drawText(shape, width*1/6-50 , height*3/6+25 , 'colour', Math.min(width, height) * 0.3);
  
     else
   if (y < height && x < width/3)
- drawText(shape, width*1/6-50 , height-10 , 'red', Math.min(width, height) * 0.3);
+ drawText(shape, width*1/6-50 , height-10 , 'colour', Math.min(width, height) * 0.3);
  
     else
   if (y < height/3 && x < width/3*2)
- drawText(shape, width*3/6-50 , height*1/6+25 , 'red', Math.min(width, height) * 0.3);
+ drawText(shape, width*3/6-50 , height*1/6+25 , colour, Math.min(width, height) * 0.3);
  
      else
   if (y < height/3 && x < width/3*2)
- drawText(shape, width*3/6-50 , height*3/6+25 , 'red', Math.min(width, height) * 0.3);
+ drawText(shape, width*3/6-50 , height*3/6+25 , 'colour', Math.min(width, height) * 0.3);
 });
  
  
@@ -41,3 +50,7 @@ drawLine(width, height/3, 0, height/3, 'black', 10)
 drawLine(width, height/3*2, 0, height/3*2, 'black', 10)
 }
 drawFrame()
+ 
+ 
+ 
+ 
