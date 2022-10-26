a741420c95072c2e6b76ee55947e557af80faf76
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-26 14:52:02 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -13,6 +13,8 @@ registerOnclick((x, y) => {
   clickCount++
   if (clickCount % 2 === 0)
    shape = 'O'
+   else
+   shape = 'X'
   if (y < height/3 && x < width/3)
    if (clickCount % 2 === 0)
   drawText(shape, width*1/6-50 , height*1/6+25, 'red', Math.min(width, height) * 0.3);
