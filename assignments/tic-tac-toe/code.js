7371aaf9f4e875b8cf5894f1330eef18a2bcc302
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-26 15:09:03 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -35,7 +35,7 @@ registerOnclick((x, y) => {
  //
     else
  if (y < height/3 && x < width/3*2)
- drawText(shape, width*3/6-50 , height*1/6+25 , colour, Math.min(width, height) * 0.3);
+ drawText(shape, width*3/6-0 , height*1/6+25 , colour, Math.min(width, height) * 0.3);
  
      else
   if (y < height/3 && x < width/3*2)
