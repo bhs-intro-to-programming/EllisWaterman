c068e38abaf13881a81f362479295e34477a43c0
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-27 15:21:29 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -29,45 +29,38 @@ registerOnclick((x, y) => {
   if (y < height / 3 && x < width / 3) {
     drawText(shape, width * 1 / 6 - 50, height * 1 / 6 + 25, colour, Math.min(width, height) * 0.3);
     xs[0][0] = shape
-    console.log(xs)
   }
   //middle left
 
   else if (y < (height / 3) * 2 && x < width / 3) {
     drawText(shape, width * 1 / 6 - 50, height * 3 / 6 + 25, colour, Math.min(width, height) * 0.3);
     xs[1][0] = shape
-    console.log(xs)
   }
   //bottom left
 
   else if (y < height && x < width / 3) {
     drawText(shape, width * 1 / 6-50 , height - 10, colour, Math.min(width, height) * 0.3); 
     xs[2][0] = shape
-    console.log(xs)
   //top middle
   }
   else if (y < height / 3 && x < width / 3 * 2) {
     drawText(shape, width * 3 / 6 - 50, height * 1 / 6 + 25, colour, Math.min(width, height) * 0.3);
     xs[1][1] = shape
-    console.log(xs)
   }
   //middle middle
   else if (y < height / 3 * 2 && x < width / 3 * 2) {
     drawText(shape, width * 3 / 6 - 50, height * 3 / 6 + 25, colour, Math.min(width, height) * 0.3);
   xs[1][2] = shape
-    console.log(xs)
   }
 //bottom middle
   else if (y < height && x < width / 3 * 2) {
     drawText(shape, width * 3 / 6 -50, height - 10, colour, Math.min(width, height) * 0.3);
     xs[2][1] = shape
-    console.log(xs) 
   }
   //top right
   else if (y < height / 3 && x < width) {
     drawText(shape, width -150, height * 1 / 6 + 25, colour, Math.min(width, height) * 0.3);
     xs[2][0] = shape
-    console.log(xs) 
   }
 
   else if (y < height / 3 * 2 && x < width) {
@@ -77,6 +70,7 @@ registerOnclick((x, y) => {
   else if (y < height && x < width) {
     drawText(shape, width - 150, height - 10, colour, Math.min(width, height) * 0.3);
   }
+  console.log(xs)
 });
 
 
