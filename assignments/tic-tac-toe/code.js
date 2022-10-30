3a7cc7c72e46cb0aac115649703f60c62b64e346
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-29 19:17:20 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -20,8 +20,10 @@ let xs = [
 ]
 const horizontalWinner = () => {
   for (let i = 0; i < 3; i++) {
-    if (xs[i][0] == null)
+    if (xs[i][0] == null){
+      console.log("cant have winner, first is null");
       return null;
+    }
     console.log(xs[i][0] == xs[i][1], xs[i][0] == xs[i][2]);
     if (xs[i][0] == xs[i][1] && xs[i][0] == xs[i][2])
       return xs[i][0];
