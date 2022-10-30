311e5c363218201f1fe3eaf97c20846751c0f435
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-30 11:40:08 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -80,10 +80,9 @@ registerOnclick((x, y) => {
 });
 
 const endGame = (message) => {
+  drawLine(rows,cols, width, height, 'black', 10)
   drawFilledRect(0, 0, width, height, '#000000aa');
   drawText(message, width / 4, height / 2, colour, size);
-      drawLine(0,0, width, height, 'black', 10)
-
   isGameOver = true;
 
 }
