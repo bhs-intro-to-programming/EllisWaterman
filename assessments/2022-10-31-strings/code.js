73023e02002c9e9f80f4b092f9e94b36766c0df0
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-10-31 15:07:00 -0700

    Updating assessments/2022-10-31-strings/code.js

--- assessments/2022-10-31-strings/code.js
+++ assessments/2022-10-31-strings/code.js
@@ -35,3 +35,7 @@ const upDownLastCharacter = (s) => {
 const firstAndLast = (s) => {
   return s.substring(0,1)+s.substring(s.length-1)
 }
+
+const swapFrontAndBack =(s) => {
+  return s.substring(s.length-1)+s.substring(0,s.length-1)+s.substring(0,1)
+}
