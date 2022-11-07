2d0d27e4e523f2016ce53dec1aab32d1ddf13289
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-07 15:21:36 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -10,9 +10,9 @@ const emptyObject = () => {
   return {}
 }
 
-const distance = (x1, x2, y1, y2) => {
-  return Math.sqrt(Math.abs((x1['x'] - y1['y'])**2)
-  +Math.sqrt(Math.abs(x2['x'] - y2['y']))**2)
+const distance = (x, y) => {
+  return Math.sqrt(Math.abs((x.x - y.y)**2)
+  +Math.sqrt(Math.abs(x.x - y.y))**2)
 }
 
 const midpoint = () => {
