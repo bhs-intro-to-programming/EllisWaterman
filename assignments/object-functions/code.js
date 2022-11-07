f356557ea9a5918470e23fae819ecb194e7f4b83
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-07 15:15:55 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -11,7 +11,7 @@ const emptyObject = () => {
 }
 
 const distance = (x1, x2, y1, y2) => {
-  return Math.sqrt(Math.abs(x1.x - y1.y)
+  return Math.sqrt(Math.abs(x1 - y1)
   +Math.sqrt(Math.abs(x2 - y2)))
 }
 
