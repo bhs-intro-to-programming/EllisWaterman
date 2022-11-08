651a01103aa1385ac21c5391c46ee0e91212da7f
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-08 14:52:42 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -31,7 +31,7 @@ const sumSalaries = (array) => {
 
 const newHighScore = (currentHighScore, scores) => {
   for (let i = 0; i < scores.length; i++) {
-    scores[i].score
+    Math.max(scores[i].score)
   }
   return currentHighScore
 }
