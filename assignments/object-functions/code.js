c7b8d0c5ff65dc7ab568e1b523bee4b802588fb8
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-08 14:41:10 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -1,9 +1,9 @@
-const getX =(object) => {
+const getX = (object) => {
   return object.x
 }
 
 const point = (x, y) => {
-  return {x: x, y: y}
+  return { x: x, y: y }
 }
 
 const emptyObject = () => {
@@ -11,24 +11,28 @@ const emptyObject = () => {
 }
 
 const distance = (p1, p2) => {
-  return Math.sqrt(Math.abs(p1.x - p2.x)**2+Math.abs(p1.y - p2.y)**2)	
+  return Math.sqrt(Math.abs(p1.x - p2.x) ** 2 + Math.abs(p1.y - p2.y) ** 2)
 }
 
 const midpoint = (ob1, ob2) => {
-  let result = { x : 0, y : 0}
-  result.x = (ob1.x + ob2.x) /2
-  result.y = (ob1.y + ob2.y) /2 
+  let result = { x: 0, y: 0 }
+  result.x = (ob1.x + ob2.x) / 2
+  result.y = (ob1.y + ob2.y) / 2
   return result
 }
 
 const sumSalaries = (array) => {
   let sum = 0
-  for (let i = 0; i <array.length; i++) {
-  sum + array[i.salary]
+  for (let i = 0; i < array.length; i++) {
+    sum + array[i.salary]
   }
   return sum
 }
 
-const newHighScore =(currentHighScore, scores) => {
+const newHighScore = (currentHighScore, scores) => {
   f
+}
+
+const summarizeBooks = (books) => {
+
 }
