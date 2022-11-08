bf7d931efc523e83e6bb782502b12fb341dcbd69
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-08 14:43:38 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -24,7 +24,7 @@ const midpoint = (ob1, ob2) => {
 const sumSalaries = (array) => {
   let sum = 0
   for (let i = 0; i < array.length; i++) {
-    sum + array.salary[i]
+    sum + array.salary[i+1]
   }
   return sum
 }
