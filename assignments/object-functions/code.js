10aa4d7428c4bfa2044f2fb3d7fb12ab2811107b
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-10 14:40:42 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -16,8 +16,8 @@ const distance = (p1, p2) => {
 
 const midpoint = (p1, p2) => {
   let result = { x: 0, y: 0 }
-  result.x = (p1.x + p2.x) / 2
-  result.y = (p1.y + p2.y) / 2
+  result.x = (p1 + p2) / 2
+  result.y = (p1 + p2) / 2
   return result
 }
 
