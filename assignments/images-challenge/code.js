94ac4eb7cc962fec721f2e3cef887c66dbb61ce2
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-10 15:04:26 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -213,7 +213,7 @@ const drawPyramid = () => {
     for(let pos = 0; pos < i; pos++) {
       drawFilledRect(pos*20+width/2, pos*20, 25,25,'black');
       drawFilledRect(-pos*20+width/2, pos*20, 25,25,'black');
-      drawFilledRect(-pos*20, height, 25,25,'black');
+      drawFilledRect(-pos*20, 300, 25,25,'black');
 
 
     }
