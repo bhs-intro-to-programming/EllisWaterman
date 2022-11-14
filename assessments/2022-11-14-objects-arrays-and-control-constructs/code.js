196fce289c32e47cc7fec62ee3b2bc131c2b3b69
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-14 15:32:10 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -42,7 +42,7 @@ const isWinner = (player) => {
 const updateWins = (players) => {
   for (let i = 0; i < players.length; i++) {
     if (players[i].score > 100) {
-      players.wins++
+      players[2].wins++
     }
   }
   console.log(players[2].wins)
