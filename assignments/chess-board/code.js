9437d38063d373338ef2cce5a74b167321084187
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-15 15:00:17 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -1,31 +1,43 @@
-const WHITE_KING   = '♔';
-const WHITE_QUEEN  = '♕';
-const WHITE_ROOK   = '♖';
+const WHITE_KING = '♔';
+const WHITE_QUEEN = '♕';
+const WHITE_ROOK = '♖';
 const WHITE_BISHOP = '♗';
 const WHITE_KNIGHT = '♘';
-const WHITE_PAWN   = '♙';
-const BLACK_KING   = '♚';
-const BLACK_QUEEN  = '♛';
-const BLACK_ROOK   = '♜';
+const WHITE_PAWN = '♙';
+const BLACK_KING = '♚';
+const BLACK_QUEEN = '♛';
+const BLACK_ROOK = '♜';
 const BLACK_BISHOP = '♝';
 const BLACK_KNIGHT = '♞';
-const BLACK_PAWN   = '♟';
+const BLACK_PAWN = '♟';
 
 // Example of drawing one of the pieces
-drawText(BLACK_QUEEN, width/2, height/2, 'black', 128);
-
 const board = () => {
-    let s = 35
-    let colsize = Math.floor(9)
-    let rowsize = Math.floor(8)
-    for (let rows = 0; rows < rowsize; rows++) {
-      for (let cols = 0; cols < colsize; cols++) {
-        let color = (cols % 2 === 0) ? 'black' : 'grey';
-        if (rows % 2 === 0)
-          drawFilledRect(rows * s, cols * s, s, s, color)
-        else
-          drawFilledRect(rows * s, cols * s - s, s, s, color)
-      }
+  let s = 35
+  let colsize = Math.floor(9)
+  let rowsize = Math.floor(8)
+  for (let rows = 0; rows < rowsize; rows++) {
+    for (let cols = 0; cols < colsize; cols++) {
+      let color = (cols % 2 === 0) ? 'black' : 'grey';
+      if (rows % 2 === 0)
+        drawFilledRect(rows * s, cols * s, s, s, color)
+      else
+        drawFilledRect(rows * s, cols * s - s, s, s, color)
     }
   }
+}
 board()
+
+const peiceArray = [
+  [(0, 35*1), (0,35*2), (0,35*3), (0,35*4), p5, p6, p7, p8],
+  [p1, p2, p3, p4, p5, p6, p7, p8],
+  [p1, p2, p3, p4, p5, p6, p7, p8],
+  [p1, p2, p3, p4, p5, p6, p7, p8],
+  [p1, p2, p3, p4, p5, p6, p7, p8],
+  [p1, p2, p3, p4, p5, p6, p7, p8],
+  [p1, p2, p3, p4, p5, p6, p7, p8],
+  [p1, p2, p3, p4, p5, p6, p7, p8]
+]
+
+drawText(BLACK_QUEEN, peiceArray[0][0], 'black', 128);
+
