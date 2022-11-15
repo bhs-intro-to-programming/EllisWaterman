38dd93787ea93f37fa032d46d1940479e082f633
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-15 14:44:54 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -13,3 +13,19 @@ const BLACK_PAWN   = '♟';
 
 // Example of drawing one of the pieces
 drawText(BLACK_QUEEN, width/2, height/2, 'black', 128);
+
+const board = () => {
+    let s = 25
+    let colsize = Math.floor(width / s)
+    let rowsize = Math.floor(height / s)
+    for (let rows = 0; rows < rowsize; rows++) {
+      for (let cols = 0; cols < colsize; cols++) {
+        let color = (cols % 2 === 0) ? 'blue' : 'red';
+        if (rows % 2 === 0)
+          drawFilledRect(rows * s, cols * s, s, s, color)
+        else
+          drawFilledRect(rows * s, cols * s - s, s, s, color)
+      }
+    }
+  }
+board()
