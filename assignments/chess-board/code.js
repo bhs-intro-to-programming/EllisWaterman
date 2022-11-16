6531d43aedabc0aa58b84a108fe7b5848c06d2b5
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-16 14:41:34 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -15,25 +15,47 @@ const BLACK_PAWN = '♟';
 
 const playerWhite = {
 pieces: [ {K: {row : 1, col : 5}},
-          {KN: {row : 1, col : 4}}, 
+          {KN: {row : 1, col : 4}},
+          {KB: {row : 1, col : 1}},
+          {KR: {row : 1, col : 1}},
+          {Q: {row : 1, col : 1}},
+          {QN: {row : 1, col : 1}},
+          {QB: {row : 1, col : 1}},
+          {QR: {row : 1, col : 1}},
+          {P1: {row : 1, col : 1}},
+          {P2: {row : 1, col : 2}},
+          {P3: {row : 1, col : 3}},
+          {P4: {row : 1, col : 4}},
+          {P5: {row : 1, col : 5}},
+          {P6: {row : 1, col : 6}},
+          {P7: {row : 1, col : 7}},
+          {P8: {row : 1, col : 8}},
 ]
 }
 
-
-const pieceArray = [
-  [0, 35 * 1], [0, 35 * 2], [0, 35 * 3], [0, 35 * 4], [0, 35 * 5], [0, 35 * 6], [0, 35 * 7], [0, 35 * 8]
-  // [p1, p2, p3, p4, p5, p6, p7, p8],
-  // [p1, p2, p3, p4, p5, p6, p7, p8],
-  // [p1, p2, p3, p4, p5, p6, p7, p8],
-  // [p1, p2, p3, p4, p5, p6, p7, p8],
-  // [p1, p2, p3, p4, p5, p6, p7, p8],
-  // [p1, p2, p3, p4, p5, p6, p7, p8],
-  // [p1, p2, p3, p4, p5, p6, p7, p8]
+const playerBlack = {
+pieces: [ {K: {row : 1, col : 5}},
+          {KN: {row : 1, col : 4}},
+          {KB: {row : 1, col : 1}},
+          {KR: {row : 1, col : 1}},
+          {Q: {row : 1, col : 1}},
+          {QN: {row : 1, col : 1}},
+          {QB: {row : 1, col : 1}},
+          {QR: {row : 1, col : 1}},
+          {P1: {row : 1, col : 1}},
+          {P2: {row : 1, col : 2}},
+          {P3: {row : 1, col : 3}},
+          {P4: {row : 1, col : 4}},
+          {P5: {row : 1, col : 5}},
+          {P6: {row : 1, col : 6}},
+          {P7: {row : 1, col : 7}},
+          {P8: {row : 1, col : 8}},
 ]
-
-    drawText(BLACK_KING, width/2, height/2, 'black', 64);
-  
-
+}
+for(let row = 0; row<; row = row + 35) {
+ drawText(BLACK_KING, row, height/2, 'black', 64);
+}.
+drawText(BLACK_KING, width/2, height/2, 'black', 64);  
 
 const board = () => {
   let s = 35
