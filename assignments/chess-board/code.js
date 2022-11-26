2038775875403d674d4384100659820479e4498c
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-26 13:02:38 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -76,7 +76,7 @@ const board = () => {
         drawFilledRect(rows * SQUARE_SIZE, cols * SQUARE_SIZE - SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, color)
     }
   }
-  drawFilledRect(0,100,100,100, 'black')
+  drawFilledRect(0,200,100,100, 'black')
 };
 
 board();
