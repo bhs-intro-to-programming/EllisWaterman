9c304bb30f1bd8b8058ba49e0d35411a514d5c58
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-26 12:19:36 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -74,7 +74,7 @@ const board = () => {
 board();
 
 const placePiece = (piece) => {
-  drawText(piece.icon, piece.col * SQUARE_SIZE, piece.row * SQUARE_SIZE, 'black', 50);
+  drawText(piece.icon, piece.col * SQUARE_SIZE, piece.row * SQUARE_SIZE, 'black', SQUARE_SIZE);
 }
 
 
