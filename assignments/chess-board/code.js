c3d1ad306af068122aac3d231097aabaa7dc628a
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-26 13:08:04 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -42,7 +42,7 @@ const playerBlack = {
 
 const playerWhite = {
  pieces: [{ icon: WHITE_KING, row: 7, col: 4 },
- { icon: WHITE_KNIGHT, row: 7, col: 6 },
+ { icon: WHITE_KNIGHT, row: 8, col: 6 },
  { icon: WHITE_KNIGHT, row: 7, col: 1 },
  { icon: WHITE_BISHOP, row: 7, col: 2 },
  { icon: WHITE_BISHOP, row: 7, col: 5 },
