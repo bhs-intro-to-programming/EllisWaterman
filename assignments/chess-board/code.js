ff26947e570110f99dcf4ffdabe42f4de4cc0553
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-26 12:28:42 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -60,8 +60,8 @@ const playerBlack = {
 const board = () => {
   let colsize = 8;
   let rowsize = 8;
-  for (let rows = 0; rows < rowsize-1; rows++) {
-    for (let cols = 0; cols < colsize; cols++) {
+  for (let rows = 0; rows < rowsize; rows++) {
+    for (let cols = 0; cols < colsize+1; cols++) {
       let color = (cols % 2 === 0) ? 'grey' : 'transparent';
       if (rows % 2 === 0)
         drawFilledRect(rows * SQUARE_SIZE, cols * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, color)
