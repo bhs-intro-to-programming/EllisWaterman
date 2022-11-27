5a1934a4837f03a3854922ae6941f19c90d05021
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-27 13:35:05 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -95,7 +95,6 @@ let pieceSelected = null;
 registerOnclick((x, y) => {
   let col = Math.floor(x/SQUARE_SIZE)
   let row = Math.floor(y/SQUARE_SIZE)
- console.log(board[col][row])
  if (board[col][row] !== 0 && pieceSelected === null) {
   pieceSelected = {col: col, row: row, icon: board[col][row]}
  } else if (pieceSelected && board[col][row] === 0) {
