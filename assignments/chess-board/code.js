0d761ccefc1c9a002f46d9da3eae0fe47579e191
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-28 14:50:49 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -10,7 +10,7 @@ const BLACK_ROOK = '♜';
 const BLACK_BISHOP = '♝';
 const BLACK_KNIGHT = '♞';
 const BLACK_PAWN = '♟';
-const SQUARE_SIZE = 30;
+const SQUARE_SIZE = 35;
 
 // Example of drawing one of the pieces
 
