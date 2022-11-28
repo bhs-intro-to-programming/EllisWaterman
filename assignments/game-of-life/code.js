dafced049a231d559d0e5d85e126c6ac91f81ede
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-11-28 15:07:44 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -6,8 +6,8 @@ const cells = () => {
     for (let xposition = 0; xposition < xsize; xposition++) {
       if (Math.random() > (1 - .23))
       drawFilledRect(xposition * cellsize, yposition * cellsize, cellsize, cellsize, 'black')
-      animate()
+      
     }
   }
 }
-cells()
+animate(cells)
