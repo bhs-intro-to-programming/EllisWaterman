a13e9321fa5eb73fee8ff43ea17c503a9c6180d3
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-12-04 22:44:02 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -57,7 +57,7 @@ const fillTimesTable = (table) => {
 
 const sums = (n) => {
  let sumArray = [0]
- for(let i = 1; i < n; i++) {
+ for(let i = 1; i < n+1; i++) {
    let previus = sumArray[i-1]
    sumArray.push(i+previus)
  }
