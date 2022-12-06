bfecfe17d9bf197b49e0c3b75b54b3eb0856aae2
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-12-06 14:38:28 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -1,14 +1,17 @@
-const cellArray = new Array(width).fill(0).map(() => new Array(height).fill(0));
+
+
+let cellsize = 30;
+const cellArray = new Array(width/cellsize).fill(0).map(() => new Array(height/cellsize).fill(0));
+
 
 const cells = () => {
-  let cellsize = 30;
   let xsize = width/cellsize
   let ysize = height/cellsize
   for (let yposition = 0; yposition < ysize; yposition++) {
     for (let xposition = 0; xposition < xsize; xposition++) {
       if (Math.random() > (1 - .23))
       drawFilledRect(xposition * cellsize, yposition * cellsize, cellsize, cellsize, 'black')
-      console.log(xposition,yposition)
+      cellArray(xposition,yposition)
     }
   }
 }
