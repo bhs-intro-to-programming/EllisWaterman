39db7a729d65ebfbceb9f8a2ed60429cc6338e5b
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-12-07 15:03:09 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -20,4 +20,4 @@ const cells = () => {
 cells()
 //make a delay possibly
 //find a way to cellArray[x of block][y of block]
-drawFilledRect(width, height, 25, 25, 'blue')
+drawFilledRect(width/100, height/100, 25, 25, 'blue')
