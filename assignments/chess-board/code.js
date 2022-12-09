cd1980ecb5fc6510e3e5ef4f52269bf73a0f4c8b
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-12-09 15:17:01 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -99,25 +99,15 @@ registerOnclick((x, y) => {
   let row = Math.floor(y / SQUARE_SIZE)
   if (board[col][row] !== 0 && pieceSelected === null) {
     pieceSelected = board[col][row]
-    if (pieceSelected.team !== board[col][row].team) {
-     } else if (pieceSelected && board[col][row] === 0) {
-        drawPiece(pieceSelected.icon, col, row)
-        emptySpace(pieceSelected)
-        board[pieceSelected.col][pieceSelected.row] = 0
-        pieceSelected.row = row
-        pieceSelected.col = col
-        board[col][row] = pieceSelected
-        pieceSelected = null
-   } else if (pieceSelected.team !== board[col][row].team) {
-      } else if (pieceSelected && board[col][row] === 0) {
-        drawPiece(pieceSelected.icon, col, row)
-        emptySpace(pieceSelected)
-        board[pieceSelected.col][pieceSelected.row] = 0
-        pieceSelected.row = row
-        pieceSelected.col = col
-        board[col][row] = pieceSelected
-        pieceSelected = null
-      }
-    }
+  } else if (pieceSelected && board[col][row] === 0) {
+    drawPiece(pieceSelected.icon, col, row)
+    emptySpace(pieceSelected)
+    board[pieceSelected.col][pieceSelected.row] = 0
+    pieceSelected.row = row
+    pieceSelected.col = col
+    board[col][row] = pieceSelected
+    pieceSelected = null
+
+  }
 });
 
