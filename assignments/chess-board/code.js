6ff6023cfe33334f49e3192a2d0d203b9b39dbf4
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-12-12 15:10:16 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -106,6 +106,10 @@ registerOnclick((x, y) => {
   if (board[col][row] !== 0 && pieceSelected === null) {
     pieceSelected = board[col][row]
     highlightPeice(pieceSelected.icon,col,row,'blue')
+    let unSelected = board[col][row]
+    if (unSelected === pieceSelected) {
+      pieceSelected = null
+    }
   } else if ((pieceSelected && board[col][row] === 0) || (pieceSelected.team !== board[col][row].team)) {
     emptySpace(pieceSelected)
     drawPiece(pieceSelected.icon, col, row)
