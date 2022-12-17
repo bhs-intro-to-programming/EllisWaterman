684d1b6de1296d6e3e7a520150af3e7bbf44aa0c
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-12-17 15:27:17 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -27,57 +27,43 @@ const doTheyLive = () => {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
 
-let locations = [0, 45, 90, 135, 180, 225, 270, 315];
+      let locations = [0, 45, 90, 135, 180, 225, 270, 315];
+      let idx;
 
-  if (i=0) {
- locations.splice(locations.indexOf(315),1)
- locations.splice(locations.indexOf(0),1)
- locations.splice(locations.indexOf(45),1)
+      if (i = 0) {
+        locations.splice(locations.indexOf(315), 1)
+        locations.splice(locations.indexOf(0), 1)
+        locations.splice(locations.indexOf(45), 1)
 
-  }
-  if (i= rows) {
- locations.splice(locations.indexOf(225),1)
- locations.splice(locations.indexOf(180),1)
- locations.splice(locations.indexOf(125),1)
-  }
-  if ( i = cols) {
- locations.splice(locations.indexOf(45),1)
- locations.splice(locations.indexOf(90),1)
- locations.splice(locations.indexOf(135),1)
-  }
-  if (j=0) {
- locations.splice(locations.indexOf(315),1)
- locations.splice(locations.indexOf(270),1)
- locations.splice(locations.indexOf(225),1)
-  }
-  
-
-      if ((i === 0) || (j === 0)) {
-
-      let up = current[i - 1][j]
-      let upleft = current[i - 1][j - 1]
-      let upright = current[i - 1][j + 1]
-      let down = current[i + 1][j]
-      let downleft = current[i + 1][j + 1]
-      let downright = current[i + 1][j - 1]
-      let right = current[i][j + 1]
-      let left = current[i][j - 1]
       }
-      else if ((i === rows) || (j === cols)) {
-        current = true
+      if (i = rows) {
+        locations.splice(locations.indexOf(225), 1)
+        locations.splice(locations.indexOf(180), 1)
+        locations.splice(locations.indexOf(135), 1)
       }
-      else if (up || upleft || upright || down || downleft || downright || right || left) {
-        current[i][j] = true
+      if (i = cols) {
+        if (idx = locations.indexOf(45) > 0)
+          locations.splice(idx, 1)
+        locations.splice(locations.indexOf(90), 1)
+        if (idx = locations.indexOf(135) > 0)
+          locations.splice(locations.indexOf(135), 1)
       }
+      if (j = 0) {
+        if (idx = locations.indexOf(315) > 0)
+          locations.splice(locations.indexOf(315), 1)//
+        locations.splice(locations.indexOf(270), 1)
+        if (idx = locations.indexOf(225) > 0)
+          locations.splice(locations.indexOf(225), 1) //
+      }
+console.log(locations)
     }
   }
-}
-originalCells()
-//make a delay possibly
+  originalCells()
+  //make a delay possibly
 
 
-registerOnclick((x, y) => {
-  if (x < 150 && y < 50) {
-    animate(doTheyLive)
-  }
-});
+  registerOnclick((x, y) => {
+    if (x < 150 && y < 50) {
+      animate(doTheyLive)
+    }
+  });
