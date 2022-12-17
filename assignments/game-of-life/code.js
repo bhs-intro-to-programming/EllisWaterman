996e2d56d451bfaa1e329838f579f18221f97183
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-12-17 14:30:55 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -2,17 +2,20 @@ const CELLSIZE = 15;
 const rows = Math.floor(height / CELLSIZE)
 const cols = Math.floor(width / CELLSIZE)
 
-const cellArray =
+const current =
   Array(rows).fill().map(() =>
     Array(cols).fill().map(() => (false)));
 
+const next =
+  Array(rows).fill().map(() =>
+    Array(cols).fill().map(() => (false)));
 
 const originalCells = () => {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       if (Math.random() > (1 - .23)) {
         drawFilledRect(x * CELLSIZE, y * CELLSIZE, CELLSIZE, CELLSIZE, 'green');
-        cellArray[y][x] = true
+        current[y][x] = true
       }
     }
     drawFilledRect(0, 0, 100, 50, 'lightBlue')
@@ -23,22 +26,48 @@ const originalCells = () => {
 const doTheyLive = () => {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      let up = cellArray[i - 1][j]
-      let upleft = cellArray[i - 1][j - 1]
-      let upright = cellArray[i - 1][j + 1]
-      let down = cellArray[i + 1][j]
-      let downleft = cellArray[i + 1][j + 1]
-      let downright = cellArray[i + 1][j - 1]
-      let right = cellArray[i][j + 1]
-      let left = cellArray[i][j - 1]
+
+let locations = [0, 45, 90, 135, 180, 225, 270, 315];
+
+  if (i=0) {
+ locations.splice(locations.indexOf(315),1)
+ locations.splice(locations.indexOf(0),1)
+ locations.splice(locations.indexOf(45),1)
+
+  }
+  if (i= rows) {
+ locations.splice(locations.indexOf(225),1)
+ locations.splice(locations.indexOf(180),1)
+ locations.splice(locations.indexOf(125),1)
+  }
+  if ( i = cols) {
+ locations.splice(locations.indexOf(45),1)
+ locations.splice(locations.indexOf(90),1)
+ locations.splice(locations.indexOf(135),1)
+  }
+  if (j=0) {
+ locations.splice(locations.indexOf(315),1)
+ locations.splice(locations.indexOf(270),1)
+ locations.splice(locations.indexOf(225),1)
+  }
+  
+
       if ((i === 0) || (j === 0)) {
-        cellArray = true
+
+      let up = current[i - 1][j]
+      let upleft = current[i - 1][j - 1]
+      let upright = current[i - 1][j + 1]
+      let down = current[i + 1][j]
+      let downleft = current[i + 1][j + 1]
+      let downright = current[i + 1][j - 1]
+      let right = current[i][j + 1]
+      let left = current[i][j - 1]
       }
       else if ((i === rows) || (j === cols)) {
-        cellArray = true
+        current = true
       }
       else if (up || upleft || upright || down || downleft || downright || right || left) {
-        cellArray[i][j] = true
+        current[i][j] = true
       }
     }
   }
