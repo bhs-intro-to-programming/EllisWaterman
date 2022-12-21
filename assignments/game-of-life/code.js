14cd93be1b9f11ac7237b3cb6f4bdb4ab5037cd0
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-12-21 11:10:45 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -143,7 +143,7 @@ const getLocationsForCell = (i, j) => {
 originalCells()
 
 const go = () => {
-  setTimeout(go, 100);
+  setTimeout(go, 1000);
     doTheyLive()
     drawNext()
     current = next
