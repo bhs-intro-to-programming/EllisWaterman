e73f3f80e58e805f42383814ed9943fe822950aa
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-12-21 10:47:25 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -23,12 +23,24 @@ const originalCells = () => {
   }
 };
 
+const drawNext = () => {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (next[y][x]) {
+        drawFilledRect(x * CELLSIZE, y * CELLSIZE, CELLSIZE, CELLSIZE, 'green');
+      }
+    }
+    drawFilledRect(0, 0, 100, 50, 'lightBlue')
+    drawText('+1 Generation', 0, 30, 'black', 15)
+  }
+};
+
 const doTheyLive = () => {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       let currentCellState = current[i][j]
       let locations = getLocationsForCell(i, j)
-      let livingNeighborCount = countLivingNeighbors(locations,i,j)
+      let livingNeighborCount = countLivingNeighbors(locations, i, j)
 
       if (currentCellState) {
         // populated
@@ -50,7 +62,7 @@ const doTheyLive = () => {
   }
 }
 
-const countLivingNeighbors = (locations,i,j) => {
+const countLivingNeighbors = (locations, i, j) => {
   let livingthings = 0
   locations.forEach(function (location) {
     switch (location) {
@@ -67,7 +79,6 @@ const countLivingNeighbors = (locations,i,j) => {
           livingthings++
         break;
       case 135:
-      console.log(rows, cols, i,j)
         if (current[i + 1][j + 1])
           livingthings++
         break;
@@ -103,12 +114,12 @@ const getLocationsForCell = (i, j) => {
     locations.splice(locations.indexOf(0), 1)
     locations.splice(locations.indexOf(45), 1)
   }
-  if (i === rows-1) {
+  if (i === rows - 1) {
     locations.splice(locations.indexOf(225), 1)
     locations.splice(locations.indexOf(180), 1)
     locations.splice(locations.indexOf(135), 1)
   }
-  if (i === cols-1) {
+  if (i === cols - 1) {
     if (idx = locations.indexOf(45) > 0)
       locations.splice(idx, 1)
     locations.splice(locations.indexOf(90), 1)
@@ -133,5 +144,11 @@ registerOnclick((x, y) => {
   if (x < 150 && y < 50) {
     //animate(doTheyLive)
     doTheyLive()
+    drawNext()
+    current = next
+    next =
+      Array(rows).fill().map(() =>
+        Array(cols).fill().map(() => (false)));
+
   }
 });
