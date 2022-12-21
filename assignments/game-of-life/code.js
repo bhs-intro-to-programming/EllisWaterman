71c133ffd526d386b3397f727609f90b544c6a5d
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2022-12-21 10:29:28 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -26,41 +26,104 @@ const originalCells = () => {
 const doTheyLive = () => {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      // let locations = getLocationsForCell(i,j)
-      // let livingCells = countLiving(locations)
-      // newGrid[i][j] = livingCells > 2;
-      
-      let locations = [0, 45, 90, 135, 180, 225, 270, 315];
-      let idx;
-      if (i === 0) {
-        locations.splice(locations.indexOf(315), 1)
-        locations.splice(locations.indexOf(0), 1)
-        locations.splice(locations.indexOf(45), 1)
-      }
-      if (i === rows) {
-        locations.splice(locations.indexOf(225), 1)
-        locations.splice(locations.indexOf(180), 1)
-        locations.splice(locations.indexOf(135), 1)
-      }
-      if (i === cols) {
-        if (idx = locations.indexOf(45) > 0)
-          locations.splice(idx, 1)
-        locations.splice(locations.indexOf(90), 1)
-        if (idx = locations.indexOf(135) > 0)
-          locations.splice(locations.indexOf(135), 1)
-      }
-      if (j === 0) {
-        if (idx = locations.indexOf(315) > 0)
-          locations.splice(locations.indexOf(315), 1)//
-        locations.splice(locations.indexOf(270), 1)
-        if (idx = locations.indexOf(225) > 0)
-          locations.splice(locations.indexOf(225), 1) //
-      }
-                console.log(locations)
+      let currentCellState = current[i][j]
+      let locations = getLocationsForCell(i, j)
+      let livingNeighborCount = countLivingNeighbors(locations)
 
+      if (currentCellState) {
+        // populated
+        if (livingNeighborCount <= 1)
+          next[i][j] = false
+        else if (livingNeighborCount >= 4)
+          next[i][j] = false
+        else
+          next[i][j] = true
+      } else {
+        // empty
+        if (livingNeighborCount === 3) {
+          next[i][j] = true
+        } else {
+          next[i][j] = false
+        }
+      }
     }
   }
 }
+
+const countLivingNeighbors = (locations) => {
+  let livingthings = 0
+  locations.forEach(function (location) {
+    switch (expression) {
+      case 0:
+        if (current[i - 1][j])
+          livingthings++
+        break;
+      case 45:
+        if (current[i - 1][j + 1])
+          livingthings++
+        break;
+      case 90:
+        if (current[i][j + 1])
+          livingthings++
+        break;
+      case 135:
+        if (current[i + 1][j + 1])
+          livingthings++
+        break;
+      case 180:
+        if (current[i + 1][j])
+          livingthings++
+        break;
+      case 225:
+        if (current[i + 1][j - 1])
+          livingthings++
+        break;
+      case 270:
+        if (current[i][j - 1])
+          livingthings++
+        break;
+      case 315:
+        if (current[i - 1][j - 1])
+          livingthings++
+        break;
+      default:
+        break;
+    }
+  }
+  )
+  return livingthings
+}
+
+const getLocationsForCell = (i, j) => {
+  let locations = [0, 45, 90, 135, 180, 225, 270, 315];
+  let idx;
+  if (i === 0) {
+    locations.splice(locations.indexOf(315), 1)
+    locations.splice(locations.indexOf(0), 1)
+    locations.splice(locations.indexOf(45), 1)
+  }
+  if (i === rows) {
+    locations.splice(locations.indexOf(225), 1)
+    locations.splice(locations.indexOf(180), 1)
+    locations.splice(locations.indexOf(135), 1)
+  }
+  if (i === cols) {
+    if (idx = locations.indexOf(45) > 0)
+      locations.splice(idx, 1)
+    locations.splice(locations.indexOf(90), 1)
+    if (idx = locations.indexOf(135) > 0)
+      locations.splice(locations.indexOf(135), 1)
+  }
+  if (j === 0) {
+    if (idx = locations.indexOf(315) > 0)
+      locations.splice(locations.indexOf(315), 1)//
+    locations.splice(locations.indexOf(270), 1)
+    if (idx = locations.indexOf(225) > 0)
+      locations.splice(locations.indexOf(225), 1) //
+  }
+  return locations;
+}
+
 originalCells()
 //make a delay possibly
 
